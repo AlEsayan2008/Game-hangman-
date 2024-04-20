1dfcdfc31c08b59c02722d1861ba53a3f3ff20a0
AlEsayan2008 <167086118+AlEsayan2008@users.noreply.github.com>
2024-04-20 14:24:37 +0400

    hangman.py

---
+++ hangman.py
@@ -0,0 +1,66 @@
+import random 
+Hangman = [
+    '''
+    1 mistake
+    ''',
+    '''
+    2 mistake
+    ''',
+    '''
+    3 mistake
+    ''',
+    '''
+    4 mistake
+    ''',
+    '''
+    5 mistake
+    ''',
+    '''
+    You lose
+    '''
+    
+    
+]
+
+max_wrong = len(Hangman)
+Words = ["armenia","russia","usa","georgia","spain","portugal","france","germany","bulgary","canada","mexico","sweden","italia","ukraine"]
+word = random.choice(Words)
+so_far_from_word = "-" * len(word)
+wrong = 0
+used_letters = []
+
+while wrong < max_wrong and so_far_from_word != word:
+    print(Hangman[wrong])
+    print('You used these letters:', used_letters)
+    print("Your word like this:", so_far_from_word)
+    
+    guess = input("Please enter your letter: ")
+
+
+    while guess in used_letters:
+        print("Ypu already guess this letter:", used_letters)
+        guess = input("Please, enter another letter:")
+    used_letters.append(guess)
+
+
+    if guess in word:
+        print("Yeah you guess this letter:")
+    
+        new_far = ""
+        for i in range(len(word)):
+            if guess == word[i]:
+                new_far += guess
+            else:
+                new_far += so_far_from_word[i]
+        so_far_from_word = new_far
+        
+    else:
+        print("It's wrong letter")
+        wrong += 1
+if wrong == max_wrong:
+    
+    print("You're looser")
+else:
+    print("You guess this word")
+
+print("this word is " + word)
